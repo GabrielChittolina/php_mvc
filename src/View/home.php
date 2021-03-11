ccacd339e9760786036a634ce7989765ce9bea0a
Gabriel Chittolina <chittolina71@gmail.com>
2021-03-11 12:48:48 -0300

    Add files via upload

--- src/View/home.php
+++ src/View/home.php
@@ -1,13 +1,1 @@
-<!--<div class="card-div">-->
-<!--    <ul>-->
-<!--        <li>Nome: </li>-->
-<!--        <li>Sobrenome: </li>-->
-<!--        <li>Cpf: </li>-->
-<!--        <li>Data de Nascimento: </li>-->
-<!--        <li>Prioridade: </li>-->
-<!--    </ul>-->
-<!--    <div class="btns-div">-->
-<!--        <a id="editar-card" class="btn-editar" href="/cadastro?id=10">Editar</a>-->
-<!--        <button id="deletar-card" class="btn-deletar">Deletar</button>-->
-<!--    </div>-->
-<!--</div>-->
+<?php echo ''; ?>
